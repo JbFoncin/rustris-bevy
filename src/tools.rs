29f01b4c220f731234331c59bcfe70f9fe2b5f78
JbFoncin <jeanbaptiste.foncin@gmail.com>
2026-08-07 16:24:41 +0000

    tools to convert coordinates

---
+++ src/tools.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+use crate::rendering::background::{ GAME_HEIGHT, GAME_WIDTH };
+const INTERACTIVE_AREA_ORIGIN: (usize, usize) = (1, 1);
+
+pub struct CoordConverter {
+    win_h: f32,
+    win_w: f32,
+    pub block_size: f32
+}
+
+impl CoordConverter {
+
+    pub fn new(window: &Window) -> CoordConverter {
+        let block_size: f32 = (window.height() / GAME_HEIGHT as f32)
+                                .min(window.width() / GAME_WIDTH as f32);
+
+        CoordConverter { win_h: window.height(),
+                         win_w: window.width(),
+                         block_size: block_size }
+    }
+
+    pub fn down_left_to_center(&self, x: f32, y:f32) -> (f32, f32) {
+        (x - self.win_w / 2.0, y - self.win_h / 2.0)
+    }
+
+    pub fn playable_grid_idx_to_center(&self, row_index: usize, col_index: usize) -> (f32, f32) {
+        let down_left_coord:(f32, f32)  = 
+                ((INTERACTIVE_AREA_ORIGIN.0 + row_index) as f32 * self.block_size,
+                 (INTERACTIVE_AREA_ORIGIN.1 + col_index) as f32 * self.block_size);
+
+        self.down_left_to_center(down_left_coord.0, down_left_coord.1) 
+    }
+}
