49d2d84ce18a4a005148da6f0a89f5c7cce45991
JbFoncin <jeanbaptiste.foncin@gmail.com>
2026-08-12 13:15:19 +0000

    added clean module

---
+++ src/rendering/clean.rs
@@ -0,0 +1,56 @@
+use bevy::{ecs::system::command, prelude::*, render::view::window};
+
+use crate::{core::gamegrid::GameGrid, rendering::{background::BackGroundBlocks, 
+                                                  grid::PlayableAreaFixedBlocks, 
+                                                  shared::RenderingHistory, 
+                                                  tetromino::CurrentTetrominoBlocks}};
+
+pub fn clean_playable_fixed_grid(rendering_history_q: Query<&RenderingHistory>,
+                                 window_query: Query<&Window>,
+                                 gamegrid_query: Query<&GameGrid>,
+                                 entities_query: Query<Entity, With<PlayableAreaFixedBlocks>>,
+                                 mut commands: Commands) {
+    
+    let Ok(window) = window_query.single() else {return;};
+    let Ok(gamegrid) = gamegrid_query.single() else {return;};
+    let Ok(rendering_history) = rendering_history_q.single() else {return;};
+
+    if (window.height(), window.width()) == rendering_history.previous_screen_hw &&
+       rendering_history.previous_grid == gamegrid.grid { return; }
+
+    entities_query.iter().for_each(|entity| commands.entity(entity).despawn());
+
+}
+
+pub fn clean_current_tet(rendering_history_q: Query<&RenderingHistory>,
+                         gamegrid_query: Query<&GameGrid>,
+                         window_query: Query<&Window>,
+                         entities_query: Query<Entity, With<CurrentTetrominoBlocks>>,
+                         mut commands: Commands) {
+
+    let Ok(window) = window_query.single() else {return;};
+    let Ok(gamegrid) = gamegrid_query.single() else {return;};
+    let Ok(rendering_history) = rendering_history_q.single() else {return;};
+
+    if (window.height(), window.width()) == rendering_history.previous_screen_hw &&
+       rendering_history.previous_tet.mask == gamegrid.current_tetromino.mask &&
+       rendering_history.previous_tet_coord == gamegrid.tet_coords { return; }
+
+    entities_query.iter().for_each(|entity| commands.entity(entity).despawn());
+}    
+
+pub fn clean_background(rendering_history_q: Query<&RenderingHistory>,
+                        window_query: Query<&Window>,
+                        entities_query: Query<Entity, With<BackGroundBlocks>>,
+                        mut commands: Commands) {
+
+    let Ok(rendering_history) = rendering_history_q.single() 
+        else {return;};
+    let Ok(window) = window_query.single() else {return;};
+
+    if rendering_history.previous_screen_hw == (window.height(), window.width())
+        {return;}
+
+    entities_query.iter().for_each(|entity| commands.entity(entity).despawn());
+
+}
