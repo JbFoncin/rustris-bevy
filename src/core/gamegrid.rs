231421ca66235b447fad2ac70b939a094c604386
JbFoncin <jeanbaptiste.foncin@gmail.com>
2026-08-15 14:35:37 +0000

    fixed bugs in move validation

--- src/core/gamegrid.rs
+++ src/core/gamegrid.rs
@@ -21,7 +21,7 @@ pub struct GameGrid {
     tetrominos: Vec<Tetromino>,
     pub current_tetromino: Tetromino,
     pub grid: Grid,
-    pub tet_coords: Coord,
+    pub tet_coord: Coord,
     pub game_state: GameState
 }
 
@@ -40,9 +40,9 @@ impl Default for GameGrid{
 
         let grid: Grid = [[None; GRID_HEIGHT as usize]; GRID_WIDTH as usize];
 
-        let tet_coords: Coord = current_tetromino.get_init_coord();
+        let tet_coord: Coord = current_tetromino.get_init_coord();
 
-        GameGrid { tetrominos, current_tetromino, grid, tet_coords, 
+        GameGrid { tetrominos, current_tetromino, grid, tet_coord, 
                    game_state: GameState::Running }
     }
 }
@@ -65,11 +65,14 @@ impl GameGrid {
         }
 
         let tetromino: Tetromino = self.tetrominos.pop().unwrap();
-        let tet_coords: Coord = tetromino.get_init_coord();
+        let tet_coord: Coord = tetromino.get_init_coord();
 
-        if self.is_move_valid(tet_coords, tetromino.mask)
+        if self.is_move_valid(
+            Coord { x: 0, y: 0 }, 
+            &tet_coord, 
+            tetromino.mask)
             { self.current_tetromino = tetromino;
-              self.tet_coords = tet_coords; }
+              self.tet_coord = tet_coord; }
         else
             { self.game_state = GameState::GameOver }
 
@@ -79,19 +82,25 @@ impl GameGrid {
 
         let color: Srgba = self.current_tetromino.color;
         
-        self.current_tetromino.mask.iter().map(|c: &Coord| c + &self.tet_coords)
+        self.current_tetromino.mask.iter().map(|c: &Coord| c + &self.tet_coord)
                 .for_each(|c: Coord|
                           {self.grid[c.x as usize][c.y as usize] = Some(color)});          
     }
 
     pub fn move_tet_left(&mut self) {
-        if self.is_move_valid(Coord { x: -1, y: 0 }, self.current_tetromino.mask)
-            { self.tet_coords += Coord { x: -1, y: 0 }; }
+        if self.is_move_valid(
+            Coord { x: -1, y: 0 }, 
+            &self.tet_coord, 
+            self.current_tetromino.mask)
+            { self.tet_coord += Coord { x: -1, y: 0 }; }
     }
 
     pub fn move_tet_right(&mut self) {
-        if self.is_move_valid(Coord { x: 1, y: 0 }, self.current_tetromino.mask)
-            { self.tet_coords += Coord { x: 1, y: 0 }; }
+        if self.is_move_valid(
+            Coord { x: 1, y: 0 }, 
+            &self.tet_coord, 
+            self.current_tetromino.mask)
+            { self.tet_coord += Coord { x: 1, y: 0 }; }
     }
 
     pub fn dump_tet(&mut self) {
@@ -100,13 +109,16 @@ impl GameGrid {
         let coord_down: Coord = Coord{ x: 0, y: -1 };
 
         loop {
-            let coord_to_test: Coord = self.tet_coords + dump_coord + coord_down;
-            if self.is_move_valid(coord_to_test, self.current_tetromino.mask)
+            let coord_to_test: Coord = self.tet_coord + dump_coord + coord_down;
+            if self.is_move_valid(
+                coord_to_test, 
+                &self.tet_coord, 
+                self.current_tetromino.mask)
                 { dump_coord += coord_down; }
             else { break; }
         }
 
-        self.tet_coords += dump_coord;
+        self.tet_coord += dump_coord;
         self.fix_current_tetromino();
         self.renew_current_tetromino();
 
@@ -114,22 +126,29 @@ impl GameGrid {
 
     fn change_tet_mask(&mut self) {
 
-        if self.is_move_valid(self.tet_coords, self.current_tetromino.next_mask)
+    if self.is_move_valid(
+            Coord{ x: 0, y: -1}, 
+            &self.tet_coord,  
+            self.current_tetromino.next_mask)
             { self.current_tetromino.update_mask_and_next_one(); }
     }
 
     pub fn move_tet_down(&mut self) {
 
-        if self.is_move_valid(Coord{ x: 0, y: -1}, self.current_tetromino.mask)
-            { self.tet_coords += Coord{ x: 0, y: -1 }; }
+        if self.is_move_valid(
+            Coord{ x: 0, y: -1}, 
+            &self.tet_coord, 
+            self.current_tetromino.mask
+        )
+            { self.tet_coord += Coord{ x: 0, y: -1 }; }
 
         else { self.fix_current_tetromino(); 
                self.renew_current_tetromino(); }
     }
 
-    fn is_move_valid(&self, tet_coord: Coord, mask: &[Coord]) -> bool {
+    fn is_move_valid(&self, coord_change: Coord, tet_coord: &Coord, mask: &[Coord]) -> bool {
 
-        mask.iter().map(|c: &Coord| c + &self.tet_coords + tet_coord)
+        mask.iter().map(|c: &Coord| c + tet_coord + coord_change)
                    .all(|c: Coord| { (c.x >= 0) && (c.y >= 0) &&
                                      (c.x < GRID_WIDTH) && (c.y < GRID_HEIGHT) &&
                                      self.grid[c.x as usize][c.y as usize].is_none() })
