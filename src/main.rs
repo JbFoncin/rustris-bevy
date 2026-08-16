75cb2b61bc9d59f6ba54076049db9ea4b58f060f
JbFoncin <jeanbaptiste.foncin@gmail.com>
2026-08-16 14:46:18 +0000

    first push of  main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::prelude::*;
+use crate::{core::actions::{apply_player_action, make_tet_fall}, rendering::{background::render_background, 
+                        clean::{clean_background, 
+                                clean_current_tet, 
+                                clean_playable_fixed_grid}, 
+                        grid::render_playable_area_fixed_blocks, 
+                        shared::update_rendering_history, 
+                        tetromino::render_current_tetronimo}, startup::init};
+mod core;
+mod startup;
+mod rendering;
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
+        .add_systems(Startup, (init, render_background).chain())
+        .add_systems(Update, 
+            (
+                (
+                    make_tet_fall,
+                    apply_player_action
+                ),
+                (
+                    clean_background,
+                    clean_current_tet,
+                    clean_playable_fixed_grid
+                ),
+                (
+                    render_background,
+                    render_playable_area_fixed_blocks,
+                    render_current_tetronimo
+                )
+            ).chain()
+        )
+        .add_systems(PostUpdate, update_rendering_history)
+        .run();
 }
